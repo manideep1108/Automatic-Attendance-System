d1579c010318f2f775d454ec6b28473e4b48142d
Manideep Sridhara <manideep.sridhara@kgpian.iitkgp.ac.in>
2023-03-02 16:48:53 +0530

    First Draft

--- Face_Detect/main.py
+++ Face_Detect/main.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+
 import cv2
 from pathlib import Path
 from retinaface import RetinaFace
@@ -6,6 +8,7 @@ from pprint import pprint
 from deepface import DeepFace
 import pandas as pd
 from deepface.basemodels import VGGFace
+import shutil
 
 
 def detect_faces(img_path: str, bounding_box: bool = False):
@@ -35,7 +38,7 @@ def extract_faces(image_path: str, obj: dict, save_images: bool = False, save_pa
         cropped_img = img[facial_area[1]: facial_area[3], facial_area[0]: facial_area[2]]
         resp[key] = cropped_img
         if save_images:
-            cv2.imwrite(f'{save_path}_{key}.jpg', cropped_img)
+            cv2.imwrite(f'{save_path}/{key}.jpg', cropped_img)
     return resp
 
 
@@ -72,6 +75,56 @@ def crop_database(database_path: str, crop_database_base_path: str, crop_databas
                 resp = detect_faces(file_path)
                 extract_faces(file_path, resp["faces"], save_images=True, save_path=crop_file_path)
 
+def extract_name_from_path(path: str):
+    reverse = path[::-1]
+    for i in range(0, 2):
+        slash = reverse.find("/")
+        if slash == -1:
+            slash = reverse.find("\\")
+        if i == 0:
+            reverse = reverse[slash + 1:]
+        else:
+            reverse = reverse[:slash]
+    name = reverse[::-1]
+    return name
+
+
+def verify_face(img_path: str, db_path: str, ):
+    resp = DeepFace.find(img_path, db_path, detector_backend="retinaface", model_name="VGG-Face", enforce_detection= False)
+    cosine = resp[0].iloc[0]["VGG-Face_cosine"]
+    if cosine > 0.2:
+        return None
+    else:
+        identity = resp[0].iloc[0]["identity"]
+        return extract_name_from_path(identity)
+
+def get_attendance(img_path: str, db_path: str):
+    all_students = []
+    for i in os.listdir(db_path):
+        if os.path.isdir(os.path.join(db_path, i)):
+            all_students.append(i)
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    temp_path = os.path.join(current_dir, "temp")
+    os.mkdir(temp_path)
+    resp = detect_faces(img_path, False)
+    resp = extract_faces(img_path, resp["faces"], True, temp_path)
+    output = {}
+    present = []
+    for file in os.listdir(temp_path):
+        file_path = os.path.join(temp_path, file)
+        resp = verify_face(file_path, db_path)
+        if resp in all_students:
+            present.append(resp)
+
+    for student in all_students:
+        if student in present:
+            output[student] = "Present"
+        else:
+            output[student] = "Absent"
+    shutil.rmtree(temp_path, ignore_errors=False)
+    return output
+
 
 if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -87,6 +140,11 @@ if __name__ == "__main__":
     img4_path = os.path.join(current_dir, "Database/Narendra_Modi/NM1.jpg")
     img5_path = os.path.join(current_dir, "Database/Narendra_Modi/NM2.jpg")
     img6_path = os.path.join(current_dir, "Database/Vladimir_Putin/Putin1.jpg")
+    chris_path = os.path.join(current_dir, "chris.jpg")
+    class_pic = os.path.join(current_dir, "class_pic.jpg")
+    output = get_attendance(class_pic, database)
+    pprint(output)
+
 
     # resp = DeepFace.verify(img1_path, img2_path, model_name="Facenet", distance_metric="cosine", enforce_detection=False)
     # pprint(resp)
@@ -100,11 +158,19 @@ if __name__ == "__main__":
     # resp = DeepFace.verify(img4_path, img6_path, model_name="Facenet", detector_backend="retinaface")
     # pprint(resp)
     # modell = VGGFace.loadModel()
-    chris_path = os.path.join(current_dir, "chris.jpg")
-    resp = DeepFace.find(img_path=chris_path, db_path=database, detector_backend="retinaface", model_name="VGG-Face")
-    file = open("output.txt", "w")
-    for i in resp:
-        # save i as excel
-        i.to_csv('file2.csv')
-    file.close()
+
+    # resp = DeepFace.find(img_path=chris_path, db_path=database, detector_backend="retinaface", model_name="VGG-Face")
+    # resp = resp[0]
+    #
+    # resp = resp.iloc[0]["VGG-Face_cosine"]
+    # print(resp)
+    # #save as csv
+    # # resp.to_csv("hello.csv
+
+
+
+
+
+
+
 
